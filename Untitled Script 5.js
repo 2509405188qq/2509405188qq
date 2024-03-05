6294c2b689b07b8c6f529a6bd61399bfbad3004b
null <97299592+2509405188qq@users.noreply.github.com>
2024-03-05 13:33:49 +0800

    Update Untitled Script 5.js

--- Untitled Script 5.js
+++ Untitled Script 5.js
@@ -85,7 +85,7 @@ const url = 'http://www.ckboss.top/upamap?&token=741708861982&'+bodys;
     const request = {
         url: url,
         method: 'GET',
-        headers: {},
+        headers: {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36"},
     };
 
     return new Promise((resolve, reject) => {
