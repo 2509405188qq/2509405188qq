6ecdb141ca260c0958fe99265acffb5ed08c0f2c
null <97299592+2509405188qq@users.noreply.github.com>
2024-03-08 10:55:57 +0800

    Update 葫芦娃ck抓取.js

--- 葫芦娃ck抓取.js
+++ 葫芦娃ck抓取.js
@@ -15,7 +15,7 @@ if (isGetCookie) {
         session.headers = $request.headers;
         let text =JSON.stringify(session.headers);
   
-        $.subt = `获取会话！succes！${text}`
+        $.subt = `获取会话！succes！${text.tostring()}`
         console.log(`${.name}, ${$.subt}`)
         $.msg($.name, $.subt, '');
     })()
